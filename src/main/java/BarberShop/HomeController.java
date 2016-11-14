ab239b6add3e9f71acc8c9359550f121d1231ff3
Justinas <Justinas@justinass-air.univercity.local>
2016-11-14 06:36:44 +0100

    Commit test

--- src/main/java/BarberShop/HomeController.java
+++ src/main/java/BarberShop/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HomeController {
 
     @RequestMapping("/home")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World!!!") String name, Model model) {
         model.addAttribute("name", name);
         return "home";
     }
