549ce3a5ad2ab59aacdf834f37fb07b819d03f02
narkutejustina <nn.justina@gmail.com>
2017-01-09 14:01:14 +0100

    #23 TaskService, fixed booking controller

--- src/main/java/barbershop/controller/BookingController.java
+++ src/main/java/barbershop/controller/BookingController.java
@@ -1,6 +1,6 @@
 package barbershop.controller;
 
-import barbershop.service.BarberService;
+import barbershop.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/main/java/barbershop/service/TaskService.java
@@ -0,0 +1,35 @@
+package barbershop.service;
+
+import barbershop.dao.TaskDao;
+import barbershop.entity.Task;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nnjus on 2017-01-09.
+ */
+@Service
+@Transactional
+public class TaskService {
+    private final TaskDao dao;
+
+    public TaskService(TaskDao dao) {
+        this.dao = dao;
+    }
+
+    public List<Task> findAll() {
+        List<Task> tasks = new ArrayList<>();
+        for(Task task: dao.findAll()) {
+            tasks.add(task);
+        }
+        return tasks;
+    }
+
+    public void save(Task task) {
+        dao.save(task);
+    }
+
+}
